25144f34dcd387378a4328d8e06878894c5eb2f7
Amir <amir.dev21@gmail.com>
2022-04-10 12:57:53 -0400

    create db functions

---
+++ db_func.py
@@ -0,0 +1,71 @@
+import requests
+import json
+import sqlite3 as sq
+
+# query the db and return all records
+
+
+def show_all():
+    # connect to or create a database
+    conn = sq.connect('my_db.db')
+    # or to create a db in memory that gets wiped after close
+    # conn_temp = sq.connect(':memory:')
+
+    # create a table
+    # first create a curser
+    curs = conn.cursor()
+
+    # Query the database
+    curs.execute("SELECT rowid, * FROM customers")
+
+    data_all = curs.fetchall()
+
+    for item in data_all:
+        print(item)
+
+    # commit command to database, pushes curs.execute() to database
+    conn.commit()
+    # close connection
+    conn.close()
+
+
+# add new record to db
+def add_one(first, last, email):
+    conn = sq.connect('my_db.db')
+    curs = conn.cursor()
+    curs.execute("INSERT INTO customers VALUES (?,?,?)", (first, last, email))
+    # commit our command
+    conn.commit()
+    # close out connection
+    conn.close()
+
+
+# data_one = curs.fetchone()  # get first item in table
+# data_many = curs.fetchmany(3)
+  # returns python list
+
+
+# API
+
+# response = requests.get('http://api.open-notify.org/astros.json')
+
+
+# def jprint(obj):
+#     text = json.dumps(obj, sort_keys=True, indent=4)
+#     print(text)
+
+
+# name = response.json()['people']
+# # jprint(name)
+
+# crafts = []
+# people = []
+
+# for i in name:
+#     craft = i['craft']
+#     crafts.append(craft)
+#     person = i['name']
+#     people.append(person)
+
+# print(crafts[1])
+# print(people[1])
---
+++ my_app.py
@@ -0,0 +1,7 @@
+import db_func
+
+# add record to db
+db_func.add_one('Camina', 'Drummer', 'beltaloda@mail.com')
+
+# show db
+db_func.show_all()
